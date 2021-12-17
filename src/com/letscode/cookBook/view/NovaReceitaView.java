46d7d69e002e65bf28b56b20f6bc5c5223c5339d
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-12-16 23:37:40 -0300

    Implemented new "create" method for creating a new Receita with minimum parameters.

--- src/com/letscode/cookBook/view/NovaReceitaView.java
+++ src/com/letscode/cookBook/view/NovaReceitaView.java
@@ -9,29 +9,40 @@ public class NovaReceitaView {
     Scanner scanner;
     Receita receita;
     String nome;
+    Categoria categoria;
 
     public NovaReceitaView() {
         this.scanner = new Scanner(System.in);
     }
 
-    public void askNome() {
+    public String askNome() {
         System.out.println("Qual o nome da receita?");
         nome = scanner.nextLine();
         if (nome.isBlank()) {
             System.out.println("Nome inválido!");
             askNome();
         }
+        return nome;
     }
 
-    public void askCategoria() {
+    public Categoria askCategoria() {
         System.out.println("Qual a categoria da receita?");
         for (Categoria cat : Categoria.values()) {
-            System.out.printf("%d - %s", cat.ordinal(), cat.name());
+            System.out.printf("%d - %s\n", cat.ordinal(), cat.name());
         }
         int categoria = scanner.nextInt();
         if (categoria < 0 || categoria >= Categoria.values().length) {
             System.out.println("Categoria inválida!");
             askCategoria();
         }
+        return Categoria.values()[categoria];
+    }
+
+    public Receita create() {
+        this.nome = askNome();
+        this.categoria = askCategoria();
+
+        this.receita = new Receita(nome, categoria);
+        return receita;
     }
 }
